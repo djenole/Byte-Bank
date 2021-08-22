e5979faf9da08e26467e92937c5b55b2d4919f7b
Djenole da Silva Souza Reis - DRE <djenole.reis@iteris.com.br>
2021-08-22 20:37:33 -0300

    adicionado gitignore

--- ByteBank/03-ByteBank/Program.cs
+++ ByteBank/03-ByteBank/Program.cs
@@ -10,6 +10,30 @@ namespace _03_ByteBank
     {
         static void Main(string[] args)
         {
+            ContaCorrente contaDaVovo = new ContaCorrente();
+            contaDaVovo.titular = "Vovó Alice";
+            contaDaVovo.agencia = 863;
+            contaDaVovo.numero = 2312;
+
+            ContaCorrente contaDaMaira = new ContaCorrente();
+            contaDaVovo.titular = "Maira";
+            contaDaVovo.agencia = 2323;
+            contaDaVovo.numero = 1341;
+
+            Console.WriteLine(contaDaVovo == contaDaMaira); //false
+            //Os objetos instanciados da mesma classe não são iguais, objetos apontam para a referência de endereço na memória.
+
+
+            contaDaMaira = contaDaVovo;
+            Console.WriteLine(contaDaMaira == contaDaVovo);
+
+            contaDaVovo.saldo = 300;
+            Console.WriteLine(contaDaMaira.saldo);
+            Console.WriteLine(contaDaVovo.saldo);
+
+            Console.ReadLine();
+
+
         }
     }
 }
