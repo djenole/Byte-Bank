aa262d933f43085d5482616465ab255de91fe2b9
Djenole da Silva Souza Reis - DRE <djenole.reis@iteris.com.br>
2021-08-22 19:54:08 -0300

    Primeiro Commit, instancia de objeto e atribuição de valor aos atributos

---
+++ ByteBank/01-ByteBank/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            ContaCorrente contaDoDjenole = new ContaCorrente();
+            contaDoDjenole.titular = "Djenole";
+            contaDoDjenole.numero = 666;
+            contaDoDjenole.saldo = 1323;
+            contaDoDjenole.agencia = 34;
+            Console.WriteLine(contaDoDjenole.titular);
+            Console.WriteLine(contaDoDjenole.numero);
+            Console.WriteLine(contaDoDjenole.agencia);
+            Console.WriteLine(contaDoDjenole.saldo);
+            Console.ReadLine();
+           
+        }
+    }
+}
