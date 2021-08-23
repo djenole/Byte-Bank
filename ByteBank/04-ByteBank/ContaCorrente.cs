ed1817e2725648fab0aca3056f77ac794a525ee6
Djenole da Silva Souza Reis - DRE <djenole.reis@iteris.com.br>
2021-08-23 08:45:24 -0300

    Criado funções com mais de um parâmetro

--- ByteBank/04-ByteBank/ContaCorrente.cs
+++ ByteBank/04-ByteBank/ContaCorrente.cs
@@ -5,7 +5,7 @@ public class ContaCorrente
     public string titular;
     public int agencia;
     public int numero;
-    public double saldo = 232;
+    public double saldo = 100;
 
     public bool Sacar(double valor)
     {
@@ -16,6 +16,26 @@ public class ContaCorrente
         else
         {
             this.saldo -= valor;
+            return true;
+        }
+    }
+
+    public void Depositar(double valor)
+    {
+        this.saldo += valor;
+    }
+
+
+    public bool Transferir(double valor, ContaCorrente contaDestino)
+    {
+        if(this.saldo < valor)
+        {
+            return false;
+        }
+        else
+        {
+            this.saldo -= valor;
+            contaDestino.Depositar(valor);
             return true;
         }
     }
--- ByteBank/04-ByteBank/Program.cs
+++ ByteBank/04-ByteBank/Program.cs
@@ -14,11 +14,30 @@ namespace _04_ByteBank
             conta.titular = "Saibaba";
 
             Console.WriteLine(conta.saldo);
-            conta.Sacar(50);
+            
 
             bool resultadoSaque = conta.Sacar(50);
             Console.WriteLine(conta.saldo);
             Console.WriteLine(resultadoSaque);
+
+            conta.Depositar(500);
+            Console.WriteLine(conta.saldo);
+
+            ContaCorrente novaConta = new ContaCorrente();
+            novaConta.titular = "um novo cliente";
+
+            conta.Transferir(200, novaConta);
+
+            Console.WriteLine("saldo da conta bruno: " + conta.saldo);
+            Console.WriteLine("saldo da nova conta djenole: " + novaConta.saldo);
+            bool resultadoTransferencia = conta.Transferir(200, novaConta);
+            Console.WriteLine("valor da transferencia: " + resultadoTransferencia);
+
+            novaConta.Transferir(70, conta);
+            Console.WriteLine("saldo da conta bruno: " + conta.saldo);
+            Console.WriteLine("saldo da nova conta djenole: " + novaConta.saldo);
+
+
             Console.ReadLine();
         }
     }
